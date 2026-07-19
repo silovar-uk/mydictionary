ad7c31855ddeea12673e22de89989416aa9b0a67
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-20 04:37:22 +0900

    Remove repetitive update prompt
    * Remove repetitive update notice
    
    * Remove update prompt component
    
    * Remove obsolete update notice styles

--- src/components/UpdateNotice.tsx
+++
@@ -1,7 +0,0 @@
-import { useRegisterSW } from 'virtual:pwa-register/react'
-
-export function UpdateNotice() {
-  const { needRefresh: [needRefresh, setNeedRefresh], updateServiceWorker } = useRegisterSW()
-  if (!needRefresh) return null
-  return <div className="update-notice"><span>新しい版があります。</span><button type="button" onClick={() => void updateServiceWorker(true)}>更新する</button><button type="button" onClick={() => setNeedRefresh(false)}>あとで</button></div>
-}
